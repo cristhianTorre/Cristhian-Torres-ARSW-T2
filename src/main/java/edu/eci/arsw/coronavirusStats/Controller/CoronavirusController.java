28d40cb65a0a5e325feadb051d302625d383f243
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2021-10-28 08:54:53 -0500

    Controller

--- src/main/java/edu/eci/arsw/coronavirusStats/Controller/CoronavirusController.java
+++ src/main/java/edu/eci/arsw/coronavirusStats/Controller/CoronavirusController.java
@@ -1,17 +1,40 @@
 package edu.eci.arsw.coronavirusStats.Controller;
 
+import edu.eci.arsw.coronavirusStats.Services.CoronavirusServicesException;
+import edu.eci.arsw.coronavirusStats.Services.ICoronavirusServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class CoronavirusController {
 
+    @Autowired
+    ICoronavirusServices cs;
+
     @RequestMapping(value="/cases", method = RequestMethod.GET)
     public ResponseEntity<?> getAllCases(){
-        return null;
+        try{
+            return new ResponseEntity<>(cs.getAllCases(), HttpStatus.ACCEPTED);
+        } catch (CoronavirusServicesException e) {
+            if(e.getMessage().equals("Cases not found")){
+                return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value="/cases/{country}", method = RequestMethod.GET)
+    public ResponseEntity<?> getCasesByCountry(@PathVariable(name="country") String country){
+        try{
+            return new ResponseEntity<>(cs.getCasesByCountry(country),HttpStatus.ACCEPTED);
+        } catch (CoronavirusServicesException e) {
+            if(e.getMessage().equals("Cases not found")){
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
     }
 }
