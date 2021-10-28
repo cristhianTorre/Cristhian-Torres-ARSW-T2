49d882e944dbc8a9a4fe562bdefebe13bd9616fa
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2021-10-28 09:23:19 -0500

    despliegue en heroku

--- src/main/java/edu/eci/arsw/coronavirusStats/CoronavirusAPIApplication.java
+++ src/main/java/edu/eci/arsw/coronavirusStats/CoronavirusAPIApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"edu.eci.arsw.coronavirusAPI"})
+@ComponentScan(basePackages = {"edu.eci.arsw.coronavirusStats"})
 public class CoronavirusAPIApplication {
     public static void main (String[] args){
         SpringApplication.run(CoronavirusAPIApplication.class,args);
